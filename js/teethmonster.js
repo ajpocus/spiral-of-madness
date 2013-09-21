9f9dd492fc8996e246e0abc3bb474946f8b31bec
Austin Pocus <ajpocus@gmail.com>
2013-09-20 23:02:58 -0400

    Initial commit, Three.js boilerplate

---
+++ js/teethmonster.js
@@ -0,0 +1,53 @@
+$(function () {
+  // set the scene size
+  var WIDTH = 1024,
+    HEIGHT = 768;
+
+  // set some camera attributes
+  var VIEW_ANGLE = 45,
+    ASPECT = WIDTH / HEIGHT,
+    NEAR = 0.1,
+    FAR = 10000;
+
+  // get the DOM element to attach to
+  // - assume we've got jQuery to hand
+  var $container = $('#container');
+
+  // create a WebGL renderer, camera
+  // and a scene
+  var renderer = new THREE.WebGLRenderer();
+  var camera =
+    new THREE.PerspectiveCamera(
+      VIEW_ANGLE,
+      ASPECT,
+      NEAR,
+      FAR);
+
+  var scene = new THREE.Scene();
+  scene.add(camera);
+  camera.position.y = 300;
+  camera.position.z = 300;
+  renderer.setSize(WIDTH, HEIGHT);
+  $container.append(renderer.domElement);
+
+  // create an ambient light
+  var light =
+    new THREE.AmbientLight(0x404040);
+
+  // set its position
+  light.position.x = 0;
+  light.position.y = 50;
+  light.position.z = 130;
+
+  // add to the scene
+  scene.add(light);
+  
+  // draw!
+  renderer.render(scene, camera);
+  
+  (function animate() {
+    requestAnimationFrame(animate);
+    
+    renderer.render(scene, camera);
+  })();
+});
