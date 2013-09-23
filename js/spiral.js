bb6e2b3760b6133c0965a785b67a8ca14e0fa096
Austin Pocus <ajpocus@gmail.com>
2013-09-22 22:36:24 -0400

    Finally a split cube

--- js/spiral.js
+++ js/spiral.js
@@ -1,7 +1,9 @@
+g = null;
+
 $(function () {
   // set the scene size
-  var WIDTH = 1024,
-    HEIGHT = 768;
+  var WIDTH = 800,
+    HEIGHT = 600;
 
   // set some camera attributes
   var VIEW_ANGLE = 45,
@@ -24,8 +26,8 @@ $(function () {
 
   var scene = new THREE.Scene();
   scene.add(camera);
-  camera.position.y = 300;
   camera.position.z = 300;
+  camera.position.y = 20;
   
   renderer.setSize(WIDTH, HEIGHT);
   $container.append(renderer.domElement);
@@ -41,11 +43,69 @@ $(function () {
   // add to the scene
   scene.add(light);
   
+  // render the current camera coordinates in the corner
+  var posText = "x: " + camera.position.x +
+    " y: " + camera.position.y +
+    " z: " + camera.position.z;
+  posText.position = {
+    x: camera.position.x + WIDTH/2,
+    y: camera.position.y + HEIGHT/2,
+    z: camera.position.z + 300
+  };
+  scene.add(posText);
+  
+  // generate the first dungeon
+  (function generateDungeon() {
+    var cubeGeometry = new THREE.CubeGeometry(500, 10, 500);
+    var cubeMat = new THREE.MeshBasicMaterial({ color: 0x00CC00, wireframe: true });
+    
+    var cube = new THREE.Mesh(cubeGeometry, cubeMat);
+    
+    cubeGeometry.dynamic = true;
+    cubeGeometry.vertices.push(new THREE.Vector3(0, -5, -250));
+    cubeGeometry.vertices.push(new THREE.Vector3(0, 5, -250));
+    cubeGeometry.vertices.push(new THREE.Vector3(0, -5, 250));
+    cubeGeometry.vertices.push(new THREE.Vector3(0, 5, 250));
+    cubeGeometry.verticesNeedUpdate = true;
+    
+    cubeGeometry.faces.push(new THREE.Face3(8, 9, 10));
+    cubeGeometry.faces.push(new THREE.Face3(9, 10, 11));
+    
+    cubeGeometry.computeVertexNormals();
+    cubeGeometry.computeFaceNormals();
+    console.log(cubeGeometry.vertices);
+    
+    scene.add(cube);
+    
+    var posText = "x: " + camera.position.x +
+      " y: " + camera.position.y +
+      " z: " + camera.position.z;
+    posText.position = {
+      x: camera.position.x + WIDTH/2,
+      y: camera.position.y + HEIGHT/2,
+      z: camera.position.z + 300
+    };
+  })();
+  
   // draw!
   renderer.render(scene, camera);
   
+  var controls = new THREE.FirstPersonControls(camera);
+	controls.movementSpeed = 1000;
+	controls.lookSpeed = 0.1;
+  var clock = new THREE.Clock();
   (function animate() {
     requestAnimationFrame(animate);
+    
+    var delta = clock.getDelta();
+    controls.update(delta);
+    if (delta % 10 === 0) {
+      posText.position = {
+        x: camera.position.x + WIDTH/2,
+        y: camera.position.y + HEIGHT/2,
+        z: camera.position.z + 300
+      };
+    }
     
     renderer.render(scene, camera);
   })();
